18c75cdd8f289d3a6dbac36c7e89630c1ecf9ef4
Jhonata Patricio <jhonatanunnes@gmail.com>
2025-05-20 21:39:08 -0300

    Update app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import customtkinter as ctk
+
+# Configuração aparência
+ctk.set_appearance_mode('dark')
+
+# Criação da janela principal
+app = ctk.CTk()
+app.title('Sistema de Login')
+app.geometry('300x300')
+
+# Criação dos campos
+
+# Criação das funções de funcionalidade
+
+# Inicia o loop da aplicação
+app.mainloop()
